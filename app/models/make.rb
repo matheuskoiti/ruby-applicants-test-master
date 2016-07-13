125900f0cb7c0abff22a16826c6f9ff156bf77bf
Matheus Koiti <koiti.silva@gmail.com>
2016-07-13 12:47:54 -0300

    adding methods to find make and models

--- app/models/make.rb
+++ app/models/make.rb
@@ -1,2 +1,7 @@
 class Make < ActiveRecord::Base
+  
+  def self.by_webmotor_id(webmotors_make_id)
+    where(webmotors_id: webmotors_make_id)[0]
+  end
+
 end
--- app/models/model.rb
+++ app/models/model.rb
@@ -1,2 +1,7 @@
 class Model < ActiveRecord::Base
+
+  def self.by_make_id(make_id)
+    where(make_id: make_id)
+  end
+
 end
