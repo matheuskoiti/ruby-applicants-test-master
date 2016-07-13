b655ca1a5cb4c0a150b57859744581c0c467a156
Matheus Koiti <koiti.silva@gmail.com>
2016-07-13 12:57:18 -0300

    adding decent exposure

--- app/controllers/models_controller.rb
+++ app/controllers/models_controller.rb
@@ -1,10 +1,12 @@
 class ModelsController < ApplicationController
+  expose(:make) { Make.by_webmotor_id(params[:webmotors_make_id]) } 
+  expose(:models) { Model.by_make_id(make.id) }
   def index
     #search the models
     uri = URI("http://www.webmotors.com.br/carro/modelos")
 
     # Make request for Webmotors site
-    make = Make.where(webmotors_id: params[:webmotors_make_id])[0]
+    make = Make.by_webmotor_id(params[:webmotors_make_id])
 
     response = Net::HTTP.post_form(uri, { marca: params[:webmotors_make_id] })
     models_json = JSON.parse response.body
